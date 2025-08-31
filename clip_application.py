49de149970c031982e9536667f11e1ad8b4caecf
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2025-08-31 08:54:48 -0400

    refactor: simplified print statements of the CLIP application for competitive usage

--- clip_application.py
+++ clip_application.py
@@ -26,40 +26,45 @@ def app_callback(self, pad, info, user_data):
     """
     # Get the GstBuffer from the probe info
     buffer = info.get_buffer()
+
     # Check if the buffer is valid
     if buffer is None:
         return Gst.PadProbeReturn.OK
-    string_to_print = ""
+
+    # string_to_print = ""
+
     # Get the detections from the buffer
     roi = hailo.get_roi_from_buffer(buffer)
     detections = roi.get_objects_typed(hailo.HAILO_DETECTION)
     if len(detections) == 0:
         detections = [roi] # Use the ROI as the detection
+
     # Parse the detections
     for detection in detections:
-        track = detection.get_objects_typed(hailo.HAILO_UNIQUE_ID)
-        track_id = None
-        label = None
-        confidence = 0.0
-        for track_id_obj in track:
-            track_id = track_id_obj.get_id()
-        if track_id is not None:
-            string_to_print += f'Track ID: {track_id} '
+        # track = detection.get_objects_typed(hailo.HAILO_UNIQUE_ID)
+        # track_id = None
+        # label = None
+        # confidence = 0.0
+        # for track_id_obj in track:
+        #     track_id = track_id_obj.get_id()
+        # if track_id is not None:
+        #     string_to_print += f'Track ID: {track_id} '
         classifications = detection.get_objects_typed(hailo.HAILO_CLASSIFICATION)
         if len(classifications) > 0:
-            string_to_print += ' CLIP Classifications:'
             for classification in classifications:
                 label = classification.get_label()
                 confidence = classification.get_confidence()
-                string_to_print += f'Label: {label} Confidence: {confidence:.2f} '
-            string_to_print += '\n'
-        if isinstance(detection, hailo.HailoDetection):
-            label = detection.get_label()
-            bbox = detection.get_bbox()
-            confidence = detection.get_confidence()
-            string_to_print += f"Detection: {label} {confidence:.2f}\n"
-    if string_to_print:
-        print(string_to_print)
+                print(f"{label} {confidence:.2f}")
+                # string_to_print += f'{label} {confidence:.2f}'
+            # string_to_print += '\n'
+        # if isinstance(detection, hailo.HailoDetection):
+        #     label = detection.get_label()
+        #     bbox = detection.get_bbox()
+        #     confidence = detection.get_confidence()
+        #     string_to_print += f"Detection: {label} {confidence:.2f}\n"
+
+    # if string_to_print:
+    #     print(string_to_print)
     return Gst.PadProbeReturn.OK
 
 def main():
